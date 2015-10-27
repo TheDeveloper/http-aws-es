21fc8d5e4b17ec245fc8ddb7467a523f167c43fb
Wing Lian <wing.lian@gmail.com>
2015-10-27 09:22:24 -0700

    Improve handling of refreshing credentials, especially for EC2MetaCredentials

--- connector-es6.js
+++ connector-es6.js
@@ -83,35 +83,47 @@ class HttpAmazonESConnector extends HttpConnector {
     request.headers['presigned-expires'] = false;
     request.headers['Host'] = this.endpoint.host;
 
-    // Sign the request (Sigv4)
-    var signer = new AWS.Signers.V4(request, 'es');
-    signer.addAuthorization(this.creds, new Date());
+    var signAndSend = _.bind(function() {
+      // Sign the request (Sigv4)
+      var signer = new AWS.Signers.V4(request, 'es');
+      signer.addAuthorization(this.creds, new Date());
 
-    var send = new AWS.NodeHttpClient();
-    req = send.handleRequest(request, null, function (_incoming) {
-      incoming = _incoming;
-      status = incoming.statusCode;
-      headers = incoming.headers;
-      response = '';
+      var send = new AWS.NodeHttpClient();
+      req = send.handleRequest(request, null, function (_incoming) {
+        incoming = _incoming;
+        status = incoming.statusCode;
+        headers = incoming.headers;
+        response = '';
 
-      var encoding = (headers['content-encoding'] || '').toLowerCase();
-      if (encoding === 'gzip' || encoding === 'deflate') {
-        incoming = incoming.pipe(zlib.createUnzip());
-      }
+        var encoding = (headers['content-encoding'] || '').toLowerCase();
+        if (encoding === 'gzip' || encoding === 'deflate') {
+          incoming = incoming.pipe(zlib.createUnzip());
+        }
 
-      incoming.setEncoding('utf8');
-      incoming.on('data', function (d) {
-        response += d;
+        incoming.setEncoding('utf8');
+        incoming.on('data', function (d) {
+          response += d;
+        });
+
+        incoming.on('error', cleanUp);
+        incoming.on('end', cleanUp);
+      }, cleanUp);
+
+      req.on('error', cleanUp);
+
+      req.setNoDelay(true);
+      req.setSocketKeepAlive(true);
+    }, this);
+
+    if (this.creds.needsRefresh()) {
+      this.creds.refresh(function(err) {
+        if (!err) {
+          signAndSend();
+        }
       });
-
-      incoming.on('error', cleanUp);
-      incoming.on('end', cleanUp);
-    }, cleanUp);
-
-    req.on('error', cleanUp);
-
-    req.setNoDelay(true);
-    req.setSocketKeepAlive(true);
+    } else {
+      signAndSend();
+    }
 
     return function () {
       req.abort();
