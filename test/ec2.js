45bc880cffb4f9ac7497c8c52ae55c75af91021c
Geoff Wagstaff <geoff@gosquared.com>
2016-07-21 23:43:44 +0100

    update test scripts for new version

--- test/ec2.js
+++ test/ec2.js
@@ -2,14 +2,11 @@ var argv = require('minimist')(process.argv.slice(2));
 var endpoint = argv.endpoint;
 var region = argv.region;
 
-var AWS = require('aws-sdk');
-var credentials = new AWS.EC2MetadataCredentials();
 var esOptions = {
     hosts: endpoint,
     connectionClass: require('http-aws-es'),
     amazonES: {
-        region: region,
-        credentials: credentials 
+        region: region
     }
 };
 var es = require('elasticsearch').Client(esOptions);
--- test/lambda.js
+++ test/lambda.js
@@ -1,16 +1,12 @@
-var AWS = require('aws-sdk');
 var elasticsearch = require('elasticsearch');
 var http_aws_es = require('http-aws-es');
-
-var credentials = new AWS.EnvironmentCredentials('AWS');
 
 exports.handler = function(event, context, callback) {
     var esOptions = {
         hosts: event.endpoint,
         connectionClass: http_aws_es,
         amazonES: {
-            region: process.env.AWS_DEFAULT_REGION,
-            credentials: credentials
+            region: process.env.AWS_DEFAULT_REGION
         }
     };
     var es = elasticsearch.Client(esOptions);
