36ecfe6b65163a14b5d99aa0fa569e5affc66097
Marcus Andersson <systemvetaren@gmail.com>
2017-06-29 22:46:54 +0200

    made it work for node 4.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+'use strict';
 let nodeMajor = process.version.match(/([0-9]+)\./)[1];
 
 if (nodeMajor < 4) {
   module.exports = require('./legacy');
 } else if (nodeMajor.match(/4|6|8/)) {
+  require("regenerator-runtime/runtime");
   module.exports = require('./node'+nodeMajor);
 } else {
   module.exports = require('./node8');
